e02b7a8102e10bfebf211473e335ad17a825d4a8
ivan_grigorev <rauscral@gmail.com>
2020-05-21 05:35:34 +0300

    api_view DRF func

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
+
 
 urlpatterns = [
 
     path('snippets/', views.snipped_list),
     path('snippets/<int:pk>/', views.snipped_detail),
 ]
+
+
+urlpatterns = format_suffix_patterns(urlpatterns)
