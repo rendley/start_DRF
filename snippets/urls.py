070f386bcbe344ae8eaecf244e84d75603eca10d
ivan_grigorev <rauscral@gmail.com>
2020-05-21 07:15:17 +0300

    CBV simple without Mixin

--- snippets/urls.py
+++ snippets/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
 
-    path('snippets/', views.snipped_list),
-    path('snippets/<int:pk>/', views.snipped_detail),
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
 
 
