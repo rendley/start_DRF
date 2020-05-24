ff3fc2202f5cf221415d97f1bc4c583a6081ca24
ivan_grigorev <rauscral@gmail.com>
2020-05-24 13:17:37 +0300

    update

--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import permissions
+
 from rest_framework.generics import (
                                     ListCreateAPIView, 
                                     RetrieveUpdateDestroyAPIView,
@@ -12,13 +13,12 @@ from .permissions import IsOwnerOrReadOnly
 
 
 
-
 class SnippetList(ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    # переопределяем и добавляем usera к данным
+    # add user
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
@@ -29,11 +29,9 @@ class SnippetDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,
                             IsOwnerOrReadOnly,]
 
-
 class UserList(ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
 
 
 class UserDetail(RetrieveAPIView):
